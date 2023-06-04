410943debbe1524a733b91b8cfbf795ec4723b55
Andrea Delli <andrea.delli2001@gmail.com>
2023-06-04 18:14:48 +0200

    Create tasks inside lists

--- lists/urls.py
+++ lists/urls.py
@@ -3,9 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Lists
     path('', views.TaskListsView.as_view(), name="lists"),
-    path('new', views.CreateListView.as_view(), name="new_list"),
-    path('<int:pk>', views.TaskListView.as_view(), name="list"),
-    path('<int:list_pk>/<int:task_pk>', views.TaskDetailView.as_view(), name="task"),
-    path('my-tasks', views.MyTasksView.as_view(), name="my-tasks"),
+    path('<int:id>', views.TaskListView.as_view(), name="list"),
+    path('new', views.CreateListView.as_view(), name="list_new"),
+    path('<int:id>/edit', views.EditListView.as_view(), name="list_edit"),
+    path('<int:id>/delete', views.DeleteListView.as_view(), name="list_delete"),
+
+    # Tasks
+    path('my-lists', views.MyTasksView.as_view(), name="my-tasks"),
+    path('<int:list_id>/<int:task_id>', views.TaskDetailView.as_view(), name="task"),
+    path('<int:list_id>/new', views.CreateTaskView.as_view(), name="task_new"),
+    path('<int:list_id>/<int:task_id>/edit', views.EditTaskView.as_view(), name="task_edit"),
+    path('<int:list_id>/<int:task_id>/delete', views.DeleteTaskView.as_view(), name="task_delete"),
 ]
--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,12 @@ from django.views.generic import CreateView, ListView
 from lists.models import Task, TaskList
 
 
+#####################
+####### LISTS #######
+#####################
 class TaskListsView(ListView):
     model = TaskList
-    template_name = "tasks/lists.html"
+    template_name = "lists/lists.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,39 +20,76 @@ class TaskListsView(ListView):
 
 
 class TaskListView(ListView):
-    template_name = "tasks/task_list.html"
+    template_name = "lists/task_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['task_list'] = TaskList.objects.get(id=self.kwargs["pk"])
+        context['task_list'] = TaskList.objects.get(id=self.kwargs["id"])
         return context
 
     def get_queryset(self):
-        return Task.objects.filter(task_list=self.kwargs["pk"])
+        return Task.objects.filter(task_list=self.kwargs["id"])
 
 
-class TaskDetailView(View):
-    template = "task_detail.html"
+class CreateListView(CreateView):
+    model = TaskList
+    fields = ["title", "description", "participants"]
+    template_name = "lists/create_list.html"
+    success_url = reverse_lazy("lists")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class EditListView(View):
+    template = "lists/edit_list.html"
 
     def get(self, request, pk):
-        task = Task.objects.get(id=pk)
-        return render(request, self.template, {"task": task})
+        if not request.user.is_authenticated:
+            return redirect("login")
 
-    def post(self, request):
-        pass
+        task_list = TaskList.objects.get(id=pk)
+        if request.user != task_list.owner:
+            return redirect("lists")
 
-    def delete(self, request, pk):
-        task = Task.objects.get(id=pk)
-        task.delete()
+        return render(request, self.template, {"task_list": task_list})
+
+    def post(self, request, pk):
+        if not request.user.is_authenticated:
+            return redirect("login")
+
+        task_list = TaskList.objects.get(id=pk)
+        if request.user != task_list.owner:
+            return redirect("lists")
+
+        task_list.title = request.POST.get("title")
+        task_list.description = request.POST.get("description")
+        task_list.participants.set(request.POST.getlist("participants"))
+        task_list.save()
         return redirect("lists")
 
-    def put(self, request):
-        """ Update the task """
-        pass
+
+class DeleteListView(View):
+    def get(self, request, pk):
+        if not request.user.is_authenticated:
+            return redirect("login")
+
+        task_list = TaskList.objects.get(id=pk)
+        if request.user != task_list.owner:
+            return redirect("lists")
+
+        task_list.delete()
+        return redirect("lists")
+
+
+#####################
+####### TASKS #######
+#####################
 
 
 class MyTasksView(View):
-    template = "tasks/my_tasks.html"
+    template = "tasks/my_tasks.html"    # TODO
 
     def get(self, request):
         task_lists = TaskList.objects.filter(participants=request.user)
@@ -60,12 +100,86 @@ class MyTasksView(View):
         return render(request, self.template, {"task_lists": task_lists})
 
 
-class CreateListView(CreateView):
-    model = TaskList
-    fields = ["title", "description", "participants"]
-    template_name = "tasks/create_list.html"
-    success_url = reverse_lazy("lists")
+class TaskDetailView(View):
+    template = "tasks/task_detail.html" # TODO
+
+    def get(self, request, pk):
+        task = Task.objects.get(id=pk)
+        return render(request, self.template, {"task": task})
+
+    def post(self, request):
+        # TODO
+        pass
+
+    def delete(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.delete()
+        return redirect("lists")
+
+    def put(self, request):
+        """ Update the task """
+        # TODO
+        pass
+
+
+class CreateTaskView(CreateView):
+    model = Task
+    fields = ["title", "description", "assigned_to", "due_datetime", "attachment", "priority"]
+    template_name = "tasks/create_task.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["task_list"] = TaskList.objects.get(id=self.kwargs["list_id"])
+        return context
 
     def form_valid(self, form):
-        form.instance.owner = self.request.user
+        form.instance.task_list = TaskList.objects.get(id=self.kwargs["list_id"])
+        form.instance.created_by = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("list", kwargs={"id": self.kwargs["list_id"]})
+
+    # Also add the task to the task_list, after it is created
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            self.form_valid(form)
+            task_list = TaskList.objects.get(id=self.kwargs["list_id"])
+            task_list.tasks.add(self.object)
+            task_list.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
+
+
+class EditTaskView(View):
+    template = "tasks/edit_task.html"   # TODO
+
+    def get(self, request, list_id, task_id):
+        task = Task.objects.get(id=task_id)
+        return render(request, self.template, {"task": task})
+
+    def post(self, request, list_id, task_id):
+        task = Task.objects.get(id=task_id)
+        task.title = request.POST.get("title")
+        task.description = request.POST.get("description")
+        task.assigned_to = request.POST.get("assigned_to")
+        task.due_datetime = request.POST.get("due_datetime")
+        task.save()
+        return redirect("list", id=list_id)
+
+
+class DeleteTaskView(View):
+    def get(self, request, list_id, task_id):
+        task = Task.objects.get(id=task_id)
+        task.delete()
+        return redirect("list", id=list_id)
+
+
+class CompleteTaskView(View):
+    def get(self, request, list_id, task_id):
+        task = Task.objects.get(id=task_id)
+        task.completed = True
+        task.save()
+        return redirect("list", id=list_id)
